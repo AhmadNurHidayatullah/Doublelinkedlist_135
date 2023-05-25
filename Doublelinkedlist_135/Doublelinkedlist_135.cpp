76693932d0a6a9ad14fd3f58a45c8caf9ef8c082
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-05-25 15:37:44 +0700

    membuat step 1 new Node dan step 2 new Node mhs nim dan nama mhs

--- Doublelinkedlist_135/Doublelinkedlist_135.cpp
+++ Doublelinkedlist_135/Doublelinkedlist_135.cpp
@@ -30,5 +30,13 @@ DoubleLinkedList::DoubleLinkedList() {
 }
 
 void DoubleLinkedList::addNode() {
-
+	int nim;
+	string nm;
+	cout << "\nEnter the rool number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim; // step 2
+	newNode->name = nm; // step 2
 }
